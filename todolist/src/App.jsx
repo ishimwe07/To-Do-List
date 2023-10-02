30f85c4eb34096753275856fef1f3b802f485d95
Ishimwe Gabby <ishimwgabby@gmail.com>
2023-10-02 10:54:03 +0200

    Completed the to do app effectively.

---
+++ todolist/src/App.jsx
@@ -0,0 +1,62 @@
+import { useState,useEffect } from "react"
+import TodoComponent from "./toDoComponent"
+import Hero from "./hero"
+
+import { nanoid } from "nanoid";
+
+
+export default function App(){
+
+  const [toDoItem, setNewItem] = useState({value:""});
+  const [allToDoItems, setAllItems] = useState(
+          ()=> JSON.parse(localStorage.getItem("todos")) || []
+        );
+
+  useEffect(()=>{
+      localStorage.setItem("todos", JSON.stringify(allToDoItems));
+  },[allToDoItems])
+
+  const handleChange = (event)=> {
+    setNewItem({
+      id:nanoid(),
+      value: event.target.value,
+      checked:false
+    });
+  }
+  
+
+  const handleAdd =(event)=>{
+    event.preventDefault();
+    setAllItems((prevItems)=> [...prevItems, toDoItem]);
+    setNewItem({value:""});
+  }
+
+  const handleCheck = (id) => {
+    setAllItems(prevItems=> prevItems.map ( item => item.id === id ? {...item ,checked : !item.checked}:item ))
+  }
+
+  const handleDelete = (toDoId) => {
+    setAllItems(prevItem => prevItem.filter(item=> item.id !== toDoId))
+  }
+
+const allComponents = allToDoItems.map(el=> <TodoComponent 
+                                                  key={el.id} 
+                                                  id={el.id} 
+                                                  value={el.value} 
+                                                  handleCheck={() => handleCheck(el.id)} 
+                                                  checked={el.checked} 
+                                                  handleDelete={()=> handleDelete(el.id)}
+                                                  />)
+
+  return(
+    <>
+      <Hero handleChange={handleChange} value={toDoItem.value} handleAdd={handleAdd} />
+      <div className="ml-[25%] mt-[5%]">
+        <h1 className="mb-5 -ml-5 font-bold text-xl">My To Do List</h1>
+        <div>
+          {allComponents}
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ todolist/src/hero.jsx
@@ -0,0 +1,24 @@
+
+export default function Hero({handleChange, value, handleAdd}){
+
+    return(
+        <div className="grid justify-items-center gap-10 mt-[8%]">
+            <h1 className="text-slate-200 text-9xl">todos</h1>
+            <form className="flex justify-between w-1/2 relative" onSubmit={(event)=> handleAdd(event)}>
+                <input 
+                required
+                className="border-2 border-slate-400 active:border-slate-500 rounded-3xl pl-5 pr-3 py-3 w-full"
+                type="text" 
+                placeholder="Add todo..." 
+                value={value}
+                onChange={(event)=> handleChange(event)}
+                />
+                <button 
+                
+                className=
+                        "text-2xl font-bold bg-green-500 rounded-full text-white px-2 pb-1 absolute right-4 mt-2"
+                        >+</button>
+            </form>
+        </div>
+    )
+}
