2a83aafe11705c5562478bc7d5f34b06ff9b1820
Ishimwe Gabby <ishimwgabby@gmail.com>
2023-10-16 08:29:06 +0200

    fix: formated my codes for more and clear readability.

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,63 +1,66 @@
-import { useState,useEffect } from "react"
-import TodoComponent from "./toDoComponent"
-import Hero from "./hero"
-
+import { useState, useEffect } from "react";
+import TodoComponent from "./toDoComponent";
+import Hero from "./hero";
 import { nanoid } from "nanoid";
 
-
-export default function App(){
-
-  const [toDoItem, setNewItem] = useState({value:""});
+export default function App() {
+  const [toDoItem, setNewItem] = useState({ value: "" });
   const [allToDoItems, setAllItems] = useState(
-          ()=> JSON.parse(localStorage.getItem("todos")) || []
-        );
-        
+    () => JSON.parse(localStorage.getItem("todos")) || []
+  );
 
-  useEffect(()=>{
-      localStorage.setItem("todos", JSON.stringify(allToDoItems));
-  },[allToDoItems])
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(allToDoItems));
+  }, [allToDoItems]);
 
-  const handleChange = (event)=> {
+  const handleChange = (event) => {
     setNewItem({
-      id:nanoid(),
+      id: nanoid(),
       value: event.target.value,
-      checked:false
+      checked: false,
     });
-  }
-  
+  };
 
-  const handleAdd =(event)=>{
+  const handleAdd = (event) => {
     event.preventDefault();
-    setAllItems((prevItems)=> [...prevItems, toDoItem]);
-    setNewItem({value:""});
-  }
+    setAllItems((prevItems) => [...prevItems, toDoItem]);
+    setNewItem({ value: "" });
+  };
 
   const handleCheck = (id) => {
-    setAllItems(prevItems=> prevItems.map ( item => item.id === id ? {...item ,checked : !item.checked}:item ))
-  }
+    setAllItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === id ? { ...item, checked: !item.checked } : item
+      )
+    );
+  };
 
   const handleDelete = (toDoId) => {
-    setAllItems(prevItem => prevItem.filter(item=> item.id !== toDoId))
-  }
+    setAllItems((prevItem) => prevItem.filter((item) => item.id !== toDoId));
+  };
 
-const allComponents = allToDoItems.map(el=> <TodoComponent 
-                                                  key={el.id} 
-                                                  id={el.id} 
-                                                  value={el.value} 
-                                                  handleCheck={() => handleCheck(el.id)} 
-                                                  checked={el.checked} 
-                                                  handleDelete={()=> handleDelete(el.id)}
-                                                  />)
+  const allComponents = allToDoItems.map((el) => (
+    <TodoComponent
+      key={el.id}
+      id={el.id}
+      value={el.value}
+      handleCheck={() => handleCheck(el.id)}
+      checked={el.checked}
+      handleDelete={() => handleDelete(el.id)}
+    />
+  ));
 
-  return(
+  return (
     <>
-      <Hero handleChange={handleChange} value={toDoItem.value} handleAdd={handleAdd} />
+      <Hero
+        handleChange={handleChange}
+        value={toDoItem.value}
+        handleAdd={handleAdd}
+      />
       <div className="ml-[25%] mt-[5%]">
         <h1 className="mb-5 -ml-5 font-bold text-xl">My To Do List</h1>
-        <div>
-          {allComponents}
-        </div>
+        <div>{allComponents}</div>
       </div>
     </>
-  )
+  );
 }
